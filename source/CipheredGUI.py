750a2e71d5ee126effc6d61982df65d104e0deb7
Ibrahima <ibrahima@ibrahima-Ubuntu22.04.4>
2024-10-09 22:13:34 +0200

    Ajout du chiffrement AES avec la classe CipheredGUI

---
+++ source/CipheredGUI.py
@@ -0,0 +1,178 @@
+import logging
+
+import dearpygui.dearpygui as dpg
+
+import os
+
+from chat_client import ChatClient
+from generic_callback import GenericCallback
+
+from basic_gui import BasicGUI,DEFAULT_VALUES
+
+
+#nombre de bit d'un bloc
+TAILLE_BLOCK = 128
+#nombre d'octet
+TAILLE_OCTET = 16
+
+#itérations AES
+ITERATIONS = 100000
+
+# Sel statique
+SALT = "NeverGonnaGiveYouUp".encode()
+
+# Import AES
+from cryptography.hazmat.primitives import hashes,padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+import base64
+
+
+class CipheredGUI(BasicGUI):
+    #client sécurisé
+    # Surcharger le constructeur pour y inclure le champ self._key
+    def __init__(self) -> None:
+        super().__init__()
+        self.key = None
+
+    # Surcharger la fonction _create_connection_window() pour y inclure un champ password  
+    def _create_connection_window(self) -> None:
+        # windows about connexion
+        with dpg.window(label="Connection", pos=(200, 150), width=400, height=300, show=False, tag="connection_windows"):
+
+            for field in ["host", "port", "name"]:
+                with dpg.group(horizontal=True):
+                    dpg.add_text(field)
+                    dpg.add_input_text(
+                        default_value=DEFAULT_VALUES[field], tag=f"connection_{field}")
+            # Ajouter un champ password
+            with dpg.group(horizontal=True):
+                dpg.add_text("password")
+                dpg.add_input_text(
+                    default_value="", tag=f"connection_password", password=True)
+            dpg.add_button(label="Connect", callback=self.run_chat)
+
+    # Surcharger la fonction create() pour y inclure le changement du nom de la fenêtre pour "Secure Chat"
+    def create(self):
+        super().create()
+        #change the name of the windows 
+        dpg.set_viewport_title("Secure Chat")
+
+
+    # Surcharger la fonction run_chat() pour y inclure la récupération du mot de passe
+    def run_chat(self, sender, app_data) -> None:
+        host = dpg.get_value("connection_host")
+        port = int(dpg.get_value("connection_port"))
+        name = dpg.get_value("connection_name")
+        password = dpg.get_value("connection_password")
+        self._log.info(f"Connecting {name}@{host}:{port}")
+
+        self._callback = GenericCallback()
+
+        self._client = ChatClient(host, port)
+        self._client.start(self._callback)
+        self._client.register(name)
+
+        dpg.hide_item("connection_windows")
+        dpg.show_item("chat_windows")
+        dpg.set_value("screen", "Connecting")
+
+
+        #clé de 16 octets
+        self.key = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=TAILLE_OCTET,
+            salt=SALT,
+            iterations=ITERATIONS,
+            backend=default_backend()
+        ).derive(password.encode())
+        
+    #fonction qui chiffre un message avec pkcs7 et retourn un tuple (iv, message)
+    def encrypt(self, message):
+        '''
+        message: message à chiffrer
+
+        Chiffrer le message avec pkcs7 et retourner un tuple (iv, message_chiffré)
+
+        '''
+        # Fonction qui chiffre un message avec pkcs7
+        iv = os.urandom(TAILLE_OCTET)
+        encryptor = Cipher(
+            algorithms.AES(self.key),
+            modes.CTR(iv),
+            backend=default_backend()
+        ).encryptor()
+
+        #ajouter du padding selon la taille du bloc
+        padder = padding.PKCS7(TAILLE_BLOCK).padder()
+        padded_data = padder.update(message.encode()) + padder.finalize()
+
+        #retourner un tuple (iv, message_chiffré)
+        return (iv, encryptor.update(padded_data) + encryptor.finalize())
+    
+
+    #fonction qui déchiffre un message avec pkcs7 et retourne le message déchiffré
+    def decrypt(self, message):
+        '''
+        message: message à déchiffrer
+
+        Déchiffrer le message avec pkcs7 et retourner le message déchiffré
+        '''
+        #Récupérer l'iv depuis le tuple en base64
+        iv = base64.b64decode(message[0]['data'])
+        #Récupérer le message depuis le tuple en base64
+        message = base64.b64decode(message[1]['data'])
+        # Fonction qui déchiffre un message avec pkcs7
+        decryptor = Cipher(
+            algorithms.AES(self.key),
+            modes.CTR(iv),
+            backend=default_backend()
+        ).decryptor()
+
+        # Déchiffrer le message
+        unpadder = padding.PKCS7(TAILLE_BLOCK).unpadder()
+        data = decryptor.update(message) + decryptor.finalize()
+        #retourner le message déchiffré
+        return (unpadder.update(data) + unpadder.finalize()).decode()
+
+    def send(self, text) -> None:
+        '''
+        text: message à envoyer
+
+        Chiffre le message et l'envoie
+        '''
+        #Chiffrer le message
+        message = self.encrypt(text)
+        #Envoyer le message
+        self._client.send_message(message)
+
+
+    #fonction de réception des messages
+    def recv(self) -> None:
+        '''
+        Récupérer les messages et les afficher
+        '''
+        if self._callback is not None:
+            for user, message in self._callback.get():
+                #essai de déchiffrer le message
+                try:
+                    #Déchiffrer le message
+                    message = self.decrypt(message)
+                    #Afficher le message déchiffré
+                    self.update_text_screen(f"{user} : {message}")
+                except:
+                    #Afficher le message chiffré dans les logs
+                    self._log.error(f"Error while decrypting message: {message}")
+            self._callback.clear()
+
+
+
+    
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    # instanciate the class, create context and related stuff, run the main loop
+    client = CipheredGUI()
+    client.create()
+    client.loop()
